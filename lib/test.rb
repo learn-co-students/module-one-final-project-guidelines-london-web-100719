7f7f97675184927c8607a78882352656590d7d45
Ned-el-ch <nedelchev.niki@gmail.com>
2019-10-21 13:09:05 +0100

    starting the project

---
+++ lib/test.rb
@@ -0,0 +1,60 @@
+def main_screen
+
+    puts `clear`
+    puts "███████████████████████████████████████████████"
+    puts "█                                             █"
+    puts "█                  Deliveruu                  █"
+    puts "█                                             █"
+    puts "█             - cheaper than DHL -            █"
+    puts "█                                             █"
+    puts "███████████████████████████████████████████████"
+    puts "█                                             █"
+    puts "█                                             █"
+    puts "███████████████████████████████████████████████"
+    puts "█                                             █"
+    puts "█                                             █"
+    puts "█                                             █"
+    puts "█   1. Make a new delivery.                   █"
+    puts "█   2. Check delivery status.                 █"
+    puts "█   3. See past deliveries.                   █"
+    puts "█   4. Update delivery details.               █"
+    puts "█   5. Cancel a delivery.                     █"
+    puts "█                                             █"
+    puts "█                                             █"
+    puts "█                                             █"
+    puts "███████████████████████████████████████████████"
+    puts ""
+    puts "Enter the corresponding number:"
+    user_input = gets.chomp
+    
+end
+def blank_screen
+
+    puts `clear`
+    puts "███████████████████████████████████████████████"
+    puts "█                                             █"
+    puts "█                                             █"
+    puts "█                                             █"
+    puts "█                                             █"
+    puts "█                                             █"
+    puts "█                                             █"
+    puts "█                                             █"
+    puts "█                                             █"
+    puts "█                                             █"
+    puts "█                                             █"
+    puts "█                                             █"
+    puts "█                                             █"
+    puts "█                                             █"
+    puts "█                                             █"
+    puts "█                                             █"
+    puts "█                                             █"
+    puts "█                                             █"
+    puts "█                                             █"
+    puts "█                                             █"
+    puts "█                                             █"
+    puts "███████████████████████████████████████████████"
+    puts ""
+    puts "Enter the corresponding number:"
+    user_input = gets.chomp
+    
+end
