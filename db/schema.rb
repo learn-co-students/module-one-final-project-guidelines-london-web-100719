d8e71fd2e5a9a77bc7c0a7d2f1c32048cb369d33
AlexShom <a.shomalistos@gmail.com>
2019-10-22 14:05:15 +0100

    added username to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 4) do
 
   create_table "deliveries", force: :cascade do |t|
     t.integer "user_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "last_name"
     t.string "origin_address"
     t.string "password"
+    t.string "username"
   end
 
 end
--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,18 +1,24 @@
 class User < ActiveRecord::Base
 
-    has_many :deliveries
-    has_many :destinations, through: :deliveries
+  has_many :deliveries
+  has_many :destinations, through: :deliveries
 
-    def self.update_blank_passwords
-    
-        var = self.where(password = "")
+  def self.update_blank_passwords
+  
+    var = self.where(password = "")
 
-        var.each do |user|
-            
-            user.update({password: "pass"})
+    var.each do |user|
+      
+      user.update({password: "pass"})
 
-        end
-        
     end
+    
+  end
+
+  def log_in(username)
+    
+
+  end
 
 end
+
