a71a59ce0ca92ea6ed404205114b0e2cd017a2be
AlexShom <a.shomalistos@gmail.com>
2019-10-22 16:28:55 +0100

    improved what3words API

--- lib/models/what3words.rb
+++ lib/models/what3words.rb
@@ -3,24 +3,29 @@ require 'net/http'
 require 'json'
 require 'pry'
 
-# three_words = "feared.later.bright"
+def get_coordinates(three_words)
 
-# url = URI("https://api.what3words.com/v3/convert-to-coordinates?key=6RXCEY3L&words=#{three_words}&format=json")
+  url = URI("https://api.what3words.com/v3/convert-to-coordinates?key=6RXCEY3L&words=#{three_words}&format=json")
 
-# http = Net::HTTP.new(url.host, url.port)
-# http.use_ssl = true
-# http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-# request = Net::HTTP::Get.new(url)
+  request = Net::HTTP::Get.new(url)
 
-# response = http.request(request)
-# output = JSON.parse(response.read_body)
-                       
+  response = http.request(request)
+  output = JSON.parse(response.read_body)
+                        
 
-# lon = output["coordinates"]["lng"]
-# lat = output["coordinates"]["lat"]
+  lon = output["coordinates"]["lng"]
+  lat = output["coordinates"]["lat"]
 
-def distance loc1, loc2 #using Haversine formula.
+  coordinates = [lat, lon]
+
+end
+
+def get_distance loc1, loc2 #using Haversine formula.
+
   rad_per_deg = Math::PI/180  # PI / 180
   rkm = 6371                  # Earth radius in kilometers
   rm = rkm * 1000             # Radius in meters
@@ -35,6 +40,17 @@ def distance loc1, loc2 #using Haversine formula.
   c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
 
   (rm * c)/1000 # Delta in kilometers
+
 end
 
-puts distance [46.3625, 15.114444],[46.055556, 14.508333]
+def get_distance_between(address_one, address_two)
+
+a_one = get_coordinates(address_one)
+a_two = get_coordinates(address_two)
+dist = get_distance(a_one, a_two)
+dist.round(2).to_s + "km"
+
+end
+
+puts get_distance_between("fence.gross.bats", "salad.likening.sprinting")
+# system 'open https://what3words.com'
