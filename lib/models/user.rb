8f06c633ac6f3b485df7a681685448ca0c7cf0af
AlexShom <a.shomalistos@gmail.com>
2019-10-24 09:33:58 +0100

    deleted old code

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -3,28 +3,5 @@ class User < ActiveRecord::Base
   has_many :deliveries
   has_many :destinations, through: :deliveries
 
-    def self.update_passwords_to_pass
-    
-        User.all.each do |user|
-            
-            user.update({password: "pass"})
-
-      end
-    
-    end
-
-
-    def self.update_usernames
-    
-        @arr = %w(chargerman terminator danK ned-el-ch ShomZy weworkbad admin hughmungus vegetable kernel)
-
-        User.all.each_with_index do |user, index|
-            
-            user.update({username: "#{@arr[index]}"})
-
-        end
-        
-    end
-
 end
 
--- lib/models/what3words.rb
+++ lib/models/what3words.rb
@@ -28,27 +28,6 @@ def get_coordinates(three_words)
 
 end
 
-# def get_distance loc1, loc2 #using Haversine formula.
-
-#   rad_per_deg = Math::PI/180  # PI / 180
-#   rkm = 6371                  # Earth radius in kilometers
-#   rm = rkm * 1000             # Radius in meters
-
-#   dlat_rad = (loc2[0]-loc1[0]) * rad_per_deg  # Delta, converted to rad
-#   dlon_rad = (loc2[1]-loc1[1]) * rad_per_deg
-
-#   lat1_rad, lon1_rad = loc1.map {|i| i * rad_per_deg }
-#   lat2_rad, lon2_rad = loc2.map {|i| i * rad_per_deg }
-
-#   a = Math.sin(dlat_rad/2)**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad) * Math.sin(dlon_rad/2)**2
-#   c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
-
-#   (rm * c)/1000 # Delta in kilometers
-
-# end
-
-
-
 def get_distance_between(origin_address, dest_address)
 
     a_one = get_coordinates(origin_address)
@@ -66,5 +45,5 @@ end
 
 
 # puts get_distance_between("fence.gross.bats", "salad.likening.sprinting")
-get_coordinates("shit")
+# get_coordinates("shit")
 # system 'open https://what3words.com'
