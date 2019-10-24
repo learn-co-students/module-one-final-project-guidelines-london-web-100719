112b493d48eaf1c9348eac61af74df66138e4102
AlexShom <a.shomalistos@gmail.com>
2019-10-24 09:49:28 +0100

    removed requires again

--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,4 @@
 require 'bundler/setup'
-require 'io/console'
-require 'pry'
-require 'uri'
-require 'net/http'
-require 'json'
-require 'require_all'
-require 'tty-prompt'
-require 'tty-box'
-require 'tty-screen'
-require 'tty-progressbar'
-require 'tty-reader'
 
 Bundler.require
 
